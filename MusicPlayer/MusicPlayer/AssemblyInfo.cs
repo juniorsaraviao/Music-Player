309c0a1650ae2b580dbf1abbca64e1956b89feb3
juniorsaraviao <junior.saravia.o@uni.pe>
2021-05-30 23:53:30 -0500

    Relocate Fonts

--- MusicPlayer/MusicPlayer/AssemblyInfo.cs
+++ MusicPlayer/MusicPlayer/AssemblyInfo.cs
@@ -4,3 +4,6 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Adamina-Regular.ttf", Alias = "Adamina")]
 [assembly: ExportFont("PatuaOne-Regular.ttf", Alias = "PatuaOne")]
+[assembly: ExportFont("FontAwesome5Brands-Regular-400.otf", Alias = "FABrands-R")]
+[assembly: ExportFont("FontAwesome5Free-Regular-400.otf", Alias = "FAFree-Regular")]
+[assembly: ExportFont("FontAwesome5Free-Solid-900.otf", Alias = "FAFree-Solid")]
